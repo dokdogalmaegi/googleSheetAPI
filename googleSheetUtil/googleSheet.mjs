6987a92f28e8e622c3beb6c999f8542a4c09242b
dokdogalmaegi <clik202012@gmail.com>
2023-03-23 22:31:10 +0900

    Implement GoogleSheet class

--- googleSheetUtil/googleSheet.mjs
+++ googleSheetUtil/googleSheet.mjs
@@ -1,18 +1,48 @@
 import { google } from 'googleapis';
 
 import apiAuthJson from '../config/apiAuth.json' assert { type: 'json' };
-import sheetInfo from '../config/sheetInfo.json' assert { type: 'json' };
 
-const { client_email, private_key } = apiAuthJson;
-const { sheetId } = sheetInfo;
+const { client_email: clientEmail, private_key: privateKey } = apiAuthJson;
 
-export async function getSheet() {
-    const SHEET_API_URL = 'https://www.googleapis.com/auth/spreadsheets';
-    const authorize = new google.auth.JWT(client_email, null, private_key, [SHEET_API_URL]);
+const SHEET_API_URL = 'https://www.googleapis.com/auth/spreadsheets';
+export class GoogleSheet {
+    #sheetApi
+    #spreadSheetId
 
-    return google.sheets({
-        version: 'v4',
-        auth: authorize
-    });
+    constructor(spreadSheetId) {
+        const authorize = new google.auth.JWT(clientEmail, null, privateKey, [SHEET_API_URL]);
+
+        this.#sheetApi = google.sheets({
+            version: 'v4',
+            auth: authorize
+        });
+
+        this.#spreadSheetId = spreadSheetId;
+    }
+
+    async getValuesOf(startCell, endCell, sheetId = '') {
+        let range = `${startCell}:${endCell}`;
+
+        if (sheetId.length > 0) {
+            range = `${sheetId}!${range}`;
+        }
+
+        console.log(range);
+
+        return await this.#getValueOf(range);
+    }
+
+    async #getValueOf(range) {
+        const { data: { values } } = await this.#sheetApi.spreadsheets.values.get({
+            spreadsheetId: this.#spreadSheetId,
+            range
+        });
+
+        console.log(await this.#sheetApi.spreadsheets.values.get({
+            spreadsheetId: this.#spreadSheetId,
+            range,
+        }));
+
+        return values;
+    }
 }
-
--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import express from 'express';
 
-import { getSheet } from './googleSheetUtil/GoogleSheet.mjs';
+import sheetInfo from './config/sheetInfo.json' assert { type: 'json' };
+
+import { GoogleSheet } from './googleSheetUtil/GoogleSheet.mjs';
 import { SuccessResponseData, FailResponseData } from './util/ResponseUtil.mjs';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.post('/', (req, res) => {
-    const googleSheet = getSheet();
+app.post('/', async (req, res) => {
+    const googleSheet = new GoogleSheet(sheetInfo.spreadSheetId);
 
-    res.json(new SuccessResponseData('test', {a: 1}).json);
+    res.json(new SuccessResponseData('Success select A1:A3', await googleSheet.getValuesOf('A1', 'A3')).json);
 });
 
 const server = app.listen(port, () => {
