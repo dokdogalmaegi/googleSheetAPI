b1e0324532f5424ba1af5451d6d2d57b94fb5252
dokdogalmaegi <clik202012@gmail.com>
2023-03-23 21:31:21 +0900

    Implement response data model

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from 'express';
-import { getSheet } from './googleSheetUtil/googleSheet.mjs';
+
+import { getSheet } from './googleSheetUtil/GoogleSheet.mjs';
+import { SuccessResponseData, FailResponseData } from './util/ResponseUtil.mjs';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    
+app.post('/', (req, res) => {
+    const googleSheet = getSheet();
+
+    res.json(new SuccessResponseData('test', {a: 1}).json);
 });
 
 const server = app.listen(port, () => {
---
+++ util/ResponseUtil.mjs
@@ -0,0 +1,61 @@
+export const RESPONSE = {
+    SUCCESS: 'success',
+    FAIL: 'fail'
+}
+
+class ResponseData {
+    #status
+    #resultMsg
+    constructor(status, resultMsg) {
+        const isValidStatus = Object.entries(RESPONSE).filter(([key, value]) => value === status);
+        if (isValidStatus.length === 0) {
+            throw Error("Must be status is success or fail");
+        }
+
+        this.#status = status;
+        this.#resultMsg = resultMsg;
+    }
+
+    get json() {
+        return {
+            status: this.#status,
+            resultMsg: this.#resultMsg
+        }
+    }
+}
+
+export class SuccessResponseData extends ResponseData {
+    #data
+    constructor(resultMsg, data) {
+        super(RESPONSE.SUCCESS, resultMsg);
+
+        this.#data = data;
+    }
+
+    get json() {
+        return {
+            ...super.json,
+            data: this.#data
+        }
+    }
+}
+
+export class FailResponseData extends ResponseData {
+    #error
+    constructor(resultMsg, error) {
+        super(RESPONSE.FAIL, resultMsg);
+
+        if (!error instanceof Error) {
+            throw Error("Must be error param is Error object");
+        }
+
+        this.#error = error;
+    }
+
+    get json() {
+        return {
+            ...super.json,
+            error: this.#error
+        }
+    }
+}
