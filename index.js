e494f6ff2e4371485a7a73dae44889daaa35be25
dokdogalmaegi <clik202012@gmail.com>
2023-03-26 18:27:57 +0900

    Add selectRouter

--- index.js
+++ index.js
@@ -1,57 +1,14 @@
 import express from 'express';
 
-import sheetInfo from './config/sheetInfo.json' assert { type: 'json' };
-
-import { GoogleSheet } from './googleSheetUtil/GoogleSheet.mjs';
-import { isCell } from './util/ExcelUtil.mjs';
-import { SuccessResponseData, FailResponseData } from './util/ResponseUtil.mjs';
+import selectRouter from './router/SelectRouter.mjs';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded( {extended: false} ));
+app.use('/select', selectRouter);
 
-app.post('/select', async (req, res) => {
-    const { start, end } = req.body;
-    if (!isCell(start) || !isCell(end)) {
-        const notCellLocationValue = new FailResponseData('Must be start or end variable is cell location\nEx) A34', new Error('Must be start or end variable is cell location'));
-        return res.json(notCellLocationValue.json);
-    }
-
-    const googleSheet = new GoogleSheet(sheetInfo.spreadSheetId);
-    try {
-        const returnCellValue = new SuccessResponseData(`Success select ${start}:${end}`, await googleSheet.getValuesOf(start, end));
-
-        return res.json(returnCellValue.json);
-    } catch(error) {
-        const retrunFailValue = new FailResponseData(`Fail select ${start}:${end}`, error);
-
-        console.log(error);
-        return res.json(retrunFailValue.json);
-    }
-});
-
-app.post('/select/getBy/:sheetId', async (req, res) => {
-    const { params: { sheetId }, body: { start, end } } = req;
-    if (!isCell(start) || !isCell(end)) {
-        const notCellLocationValue = new FailResponseData('Must be start or end variable is cell location\nEx) A34', new Error('Must be start or end variable is cell location'));
-
-        return res.json(notCellLocationValue.json);
-    }
-
-    const googleSheet = new GoogleSheet(sheetInfo.spreadSheetId);
-    try {
-        const returnCellValue = new SuccessResponseData(`Success select ${start}:${end}`, await googleSheet.getValuesOf(start, end, sheetId));
-
-        return res.json(returnCellValue.json);
-    } catch(error) {
-        const retrunFailValue = new FailResponseData(`Fail select ${sheetId}!${start}:${end}`, error);
-
-        console.log(error);
-        return res.json(retrunFailValue.json);
-    } 
-});
 
 const server = app.listen(port, () => {
     console.log(`server on ${port}`);
---
+++ router/SelectRouter.mjs
@@ -0,0 +1,52 @@
+import express from 'express';
+
+import sheetInfo from '../config/sheetInfo.json' assert { type: 'json' };
+
+import { GoogleSheet } from '../googleSheetUtil/GoogleSheet.mjs';
+import { isCell } from '../util/ExcelUtil.mjs';
+import { SuccessResponseData, FailResponseData } from '../util/ResponseUtil.mjs';
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    const { start, end } = req.body;
+    if (!isCell(start) || !isCell(end)) {
+        const notCellLocationValue = new FailResponseData('Must be start or end variable is cell location\nEx) A34', new Error('Must be start or end variable is cell location'));
+        return res.json(notCellLocationValue.json);
+    }
+
+    const googleSheet = new GoogleSheet(sheetInfo.spreadSheetId);
+    try {
+        const returnCellValue = new SuccessResponseData(`Success select ${start}:${end}`, await googleSheet.getValuesOf(start, end));
+
+        return res.json(returnCellValue.json);
+    } catch(error) {
+        const retrunFailValue = new FailResponseData(`Fail select ${start}:${end}`, error);
+
+        console.log(error);
+        return res.json(retrunFailValue.json);
+    }
+});
+
+router.post('/getBy/:sheetId', async (req, res) => {
+    const { params: { sheetId }, body: { start, end } } = req;
+    if (!isCell(start) || !isCell(end)) {
+        const notCellLocationValue = new FailResponseData('Must be start or end variable is cell location\nEx) A34', new Error('Must be start or end variable is cell location'));
+
+        return res.json(notCellLocationValue.json);
+    }
+
+    const googleSheet = new GoogleSheet(sheetInfo.spreadSheetId);
+    try {
+        const returnCellValue = new SuccessResponseData(`Success select ${start}:${end}`, await googleSheet.getValuesOf(start, end, sheetId));
+
+        return res.json(returnCellValue.json);
+    } catch(error) {
+        const retrunFailValue = new FailResponseData(`Fail select ${sheetId}!${start}:${end}`, error);
+
+        console.log(error);
+        return res.json(retrunFailValue.json);
+    } 
+});
+
+export default router;
